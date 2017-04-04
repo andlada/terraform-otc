79794516665b74055c5a06255571db7528b58fca
andlada <andras.lada@t-systems.com>
2017-04-04 16:38:22 +0800

    Update provider.tf

--- terraform-otc/provider.tf
+++ terraform-otc/provider.tf
@@ -1,3 +1,11 @@
+# Configure the OpenStack Provider
+provider "openstack" {
+  user_name   = "xxxxxxxx"
+  password    = "zzzzzzzz"
+  tenant_name = "OTCyyyyyyyyyyyyyyyyyyyy"
+  auth_url    = "https://iam.eu-de.otc.t-systems.com:443/v3"
+}
+
 # Configure AWS provider to use OTC OBS (S3 compatible)
 provider "aws" {
   access_key = "${var.aws_access_key}"
